fa7a6b8564af534e3cf3be17ad834e249b86400b
Tersornpat <sornpat@gmail.com>
2023-02-16 14:54:35 +0700

    fix

--- ajwisan/pts/authen.php
+++ ajwisan/pts/authen.php
@@ -1,23 +1,22 @@
 <?php
-    // session_start();
+    session_start();
     include "./connectdb.php";
 ?>
 <?php
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $showdata = "SELECT
-                    * 	
-                    -- E_ID,	
-                    -- E_Name,	
-                    -- E_Surname,
-                    -- E_Role,
-                    -- E_Address
-                FROM pts_employee WHERE E_Username='$username' AND E_Password='$password';";
-    $result = $conn->query($showdata);
+    // $showdata = "SELECT 	
+    //                 E_ID,	
+    //                 E_Name,	
+    //                 E_Surname,
+    //                 E_Role,
+    //                 E_Address
+    //             FROM pts_employee WHERE E_Username='$username' AND E_Password='$password';";
+    // $result = $conn->query($showdata);
     $conn->close();
-    $row = $result->fetch_assoc();
-    echo $row["E_ID"]."   ".$row["E_Name"]."   ".$row["E_Surname"]."   ".$row["E_Role"]."   ".$row["Member_Tel"]."<br>";
+    // $row = $result->fetch_assoc();
+    // echo $row["E_ID"]."   ".$row["E_Name"]."   ".$row["E_Surname"]."   ".$row["E_Role"]."   ".$row["Member_Tel"]."<br>";
 
     // $rowcount = mysqli_num_rows($result);
     // $row = $result->fetch_assoc();
