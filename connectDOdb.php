d63a40cf513f69c2358c30944c2a08252dd332ea
Tersornpat <sornpat@gmail.com>
2023-02-16 21:08:02 +0700

    check2

--- connectDOdb.php
+++ connectDOdb.php
@@ -1,12 +1,11 @@
 <?php
-    $username = "doadmin";
-    $password = "";
-    $servername = "fullstackdb-do-user-13569317-0.b.db.ondigitalocean.com:25060";
-    $dbname = "test";
-    $conn = new mysqli($servername, $username, $password,$dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully <br>";
+    $host="fullstackdb-do-user-13569317-0.b.db.ondigitalocean.com";
+    $port=25060;
+    $socket="";
+    $user="doadmin";
+    $password="";
+    $dbname="test";
+
+    $conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
+        or die ('Could not connect to the database server' . mysqli_connect_error());
 ?>
