c309ca3c97a0843b8458b6b8a585f08319e8f05e
Tersornpat <sornpat@gmail.com>
2023-02-16 21:59:03 +0700

    fix

--- index.php
+++ index.php
@@ -26,18 +26,8 @@
         </tr>
     <?php
 
-    $username = "doadmin";
-    $password = "";
-    $servername = "fullstackdb-do-user-13569317-0.b.db.ondigitalocean.com:25060";
-    $dbname = "test";
-    $conn = new mysqli($servername, $username, $password,$dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully <br>";
-
-    // $query = "
+        include "connectDOdb.php";
+        // $query = "
         // CREATE TABLE `pts_employee` (
         //     `E_ID` int(11) NOT NULL Primary KEY,
         //     `E_Username` varchar(20) NOT NULL,
