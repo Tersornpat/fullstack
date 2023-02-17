d75b9ab4c75aca3aeb192e9e09e95c1f4af2f834
Tersornpat <sornpat@gmail.com>
2023-02-17 14:56:56 +0700

    update

--- index.php
+++ index.php
@@ -12,10 +12,9 @@
         <br>
         <a href="./ajakan/1"><button type="button">Button To ATM</button></a>
         <a href="./ajwisan/pts"><button type="button" style="width:200px;height;30px">Button To AJwisan pts</button></a>
-        <br>
+        <!-- <br>
         <table>
         <tr>
-            <th>11</th>
             <th>E_ID</th>
             <th>E_Username</th>
             <th>E_Password</th>
@@ -26,39 +25,24 @@
         </tr>
     <?php
 
-        include "connectDOdb.php";
-        // $query = "
-        // CREATE TABLE `pts_employee` (
-        //     `E_ID` int(11) NOT NULL Primary KEY,
-        //     `E_Username` varchar(20) NOT NULL,
-        //     `E_Password` varchar(20) NOT NULL,
-        //     `E_Name` varchar(20) NOT NULL,
-        //     `E_Surname` varchar(20) NOT NULL,
-        //     `E_Role` varchar(10) NOT NULL,
-        //     `E_Address` varchar(255) DEFAULT NULL
-        // );";
-        // $query = "INSERT INTO pts_employee VALUES (1,'sornpat2','ssss','sornpat','Amthiangtham','admin','1234 wk strt');";
+        // include "connectDOdb.php";
+        // $query = "Select * FROM pts_employee;";
+        // $result = $conn->query($query);
+        // $conn->close();
 
-        $query = "Select * FROM pts_employee;";
-        $result = $conn->query($query);
-        $conn->close();
-
-        echo [$result];
-
-        while($row = $result->fetch_assoc()) {
-            echo "  <tr>
-                        <td>1</td>
-                        <td>".$row["E_ID"]."</td>
-                        <td><b>".$row["E_Username"]."</b></td>
-                        <td>".$row["E_Password"]."</td>
-                        <td>".$row["E_Name"]."</td>
-                        <td>".$row["E_Surname"]."</td>
-                        <td>".$row["E_Role"]."</td>
-                        <td>".$row["E_Address"]."</td>
-                    </tr>   ";
-        }
+        // while($row = $result->fetch_assoc()) {
+        //     echo "  <tr>
+        //                 <td>".$row["E_ID"]."</td>
+        //                 <td><b>".$row["E_Username"]."</b></td>
+        //                 <td>".$row["E_Password"]."</td>
+        //                 <td>".$row["E_Name"]."</td>
+        //                 <td>".$row["E_Surname"]."</td>
+        //                 <td>".$row["E_Role"]."</td>
+        //                 <td>".$row["E_Address"]."</td>
+        //             </tr>   ";
+        // }
     ?>
-    </table>
+    </table> -->
     </center>
 </body>
 </html>
