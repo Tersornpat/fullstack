f24f92f8cad8d200469891216c8c3a4e3e65b74c
Tersornpat <sornpat@gmail.com>
2023-02-17 14:52:04 +0700

    bacck

--- connectDOdb.php
+++ connectDOdb.php
@@ -1,7 +1,7 @@
 <?php
     $username = "fullstack";
     $password = "1234";
-    $servername = "128.199.234.176";
+    $servername = "localhost";
     $dbname = "pts";
     $conn = new mysqli($servername, $username, $password,$dbname);
     // Check connection
