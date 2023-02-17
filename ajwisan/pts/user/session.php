322f426edc1dd5b058ca9b03bb4ddd2164b11d39
Tersornpat <sornpat@gmail.com>
2023-02-17 14:39:10 +0700

    fix

--- ajwisan/pts/user/session.php
+++ ajwisan/pts/user/session.php
@@ -3,5 +3,5 @@
     if ($_SESSION['key']!="user"){
         header('Location: /pts/index.php');
     }
-    echo "<center><a href='./index.php'><button type='submit' width=100px height=30px><font size=7>LOG OUT</font></button></a></center><br>";
+    echo "<center><a href='/ajwisan/pts/index.php'><button type='submit' width=100px height=30px><font size=7>LOG OUT</font></button></a></center><br>";
 ?>
